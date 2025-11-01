01c6cae0baa4cd00374cb12a7a5e487de56d24d9
nikitaSB01 <boss.shvabauer@bk.ru>
2025-11-01 22:13:03 +0500

    правка

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
+import SearchResultsPage from '../pages/SearchResultsPage/SearchResultsPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/search',
+    element: <SearchResultsPage />,
   },
 ]);
