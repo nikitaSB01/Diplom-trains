42fbba6656af5b6762065fc3e0f9e4128378f828
Nikita Shvabauer <halkaster666@mail.ru>
2025-11-06 18:28:22 +0500

    создал общую структуру фильтров

---
+++ src/components/SearchPageMain/FiltersLastTickets/FiltersLastTickets.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./FiltersLastTickets.module.css";
+
+const FiltersTwo: React.FC = () => {
+  return (
+    <div className={styles.block}>
+      <h3 className={styles.title}>Последние билеты</h3>
+    </div>
+  );
+};
+
+export default FiltersTwo;
---
+++ src/components/SearchPageMain/Trains/Trains.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Trains.module.css";
+
+const Trains: React.FC = () => {
+  return (
+    <div className={styles.block}>
+      <h3 className={styles.title}>Фильтры</h3>
+     
+    </div>
+  );
+};
+
+export default Trains;
