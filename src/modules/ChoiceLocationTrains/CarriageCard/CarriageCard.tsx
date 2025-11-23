2249b55663e6e4868def6a5e21eadea895fd4dc4
Nikita Shvabauer <halkaster666@mail.ru>
2025-11-24 00:01:20 +0500

    промежуточный по отображению доп услуг

--- src/modules/ChoiceLocationTrains/CarriageCard/CarriageCard.tsx
+++ src/modules/ChoiceLocationTrains/CarriageCard/CarriageCard.tsx
@@ -5,6 +5,7 @@ import { ReactComponent as AC } from "../../../assets/icons/Train/conditioning.s
 import { ReactComponent as Wifi } from "../../../assets/icons/Train/wifi.svg";
 import { ReactComponent as Food } from "../../../assets/icons/Train/cup.svg";
 import { ReactComponent as Linens } from "../../../assets/icons/Train/Underwear.svg";
+import { ReactComponent as Ruble } from "../../../assets/icons/Train/ruble.svg";
 
 import CarSeatsMap from "../CarSeatsMap/CarSeatsMap";
 
@@ -12,91 +13,108 @@ const CarriageCard = ({ carriage }: any) => {
     const coach = carriage.coach;
     const seats = carriage.seats;
 
-    const extractWagonNumber = (name: string) => {
-        const match = name.match(/\d+/g);
-        return match ? match.join("") : name;
-    };
+    const wagonNumber = coach.name.match(/\d+/)?.[0] ?? coach.name;
+
+    const type = coach.class_type;
+
+    const isLuxOrSeat = type === "first" || type === "fourth";
+    const isCoupeOrPlatz = type === "second" || type === "third";
+
+    const upperPrice = isCoupeOrPlatz ? coach.top_price : null;
+    const lowerPrice = isCoupeOrPlatz ? coach.bottom_price : coach.top_price;
 
     return (
         <div className={styles.card}>
-            {/* ПРАВАЯ ЧАСТЬ */}
             <div className={styles.container}>
-                {/* ЛЕВАЯ БОЛЬШАЯ ЖЁЛТАЯ ПАНЕЛЬ */}
+
+                {/* ЛЕВАЯ ЖЁЛТАЯ ЧАСТЬ */}
                 <div className={styles.left}>
-                    <div className={styles.wagonNumber}>{extractWagonNumber(coach.name)}</div>
+                    <div className={styles.wagonNumber}>{wagonNumber}</div>
                     <div className={styles.wagonLabel}>вагон</div>
                 </div>
+
+                {/* ПРАВАЯ ЧАСТЬ */}
                 <div className={styles.right}>
-                    {/* Ряд сверху */}
-                    <div className={styles.row}>
-                        <div className={styles.col}>
-                            <div className={styles.colTitle}>Места</div>
-                            <div className={styles.colValue}>{coach.available_seats}</div>
-                        </div>
 
+                    {/* ======= БЛОКИ 1 и 2: МЕСТА & СТОИМОСТЬ ======= */}
+                    <div className={styles.topRow}>
+                        {/* === КОЛОНКА 1: МЕСТА === */}
                         <div className={styles.col}>
-                            <div className={styles.colTitle}>Стоимость</div>
-                            <div className={styles.priceBlock}>
-                                <span>{coach.bottom_price.toLocaleString("ru-RU")}</span>
-                                <span className={styles.ruble}>₽</span>
+                            <div className={styles.placeRow}>
+                                <span className={styles.titleGrayInline}>Места</span>
+                                <span className={styles.valueBigInline}>{coach.available_seats}</span>
                             </div>
+
+                            {isCoupeOrPlatz && (
+                                <div className={styles.subInfo}>
+                                    <div className={styles.subRow}>
+                                        <span className={styles.subTitle}>Верхние</span>
+                                    </div>
+                                    <div className={styles.subRow}>
+                                        <span className={styles.subTitle}>Нижние</span>
+                                    </div>
+                                </div>
+                            )}
                         </div>
 
+                        {/* === КОЛОНКА 2: СТОИМОСТЬ === */}
                         <div className={styles.col}>
-                            <div className={styles.colTitle}>Обслуживание</div>
-                            <div className={styles.serviceIcons}>
-                                {coach.have_air_conditioning && (
-                                    <div className={styles.iconWrap}>
-                                        <AC />
-                                        <span className={styles.tooltip}>кондиционер</span>
-                                    </div>
-                                )}
+                            <div className={styles.titleGray}>Стоимость</div>
 
-                                {coach.have_wifi && (
-                                    <div className={styles.iconWrap}>
-                                        <Wifi />
-                                        <span className={styles.tooltip}>Wi-Fi</span>
-                                    </div>
-                                )}
+                            {isCoupeOrPlatz && (
+                                <div className={styles.containerPrice}>
+                                    <div className={styles.costRow}>
+                                        <span className={styles.costValue}>
+                                            {upperPrice.toLocaleString("ru-RU")}
+                                        </span>
+                                        <Ruble className={styles.rubleIcon} />
 
-                                {!coach.is_linens_included && coach.linens_price > 0 && (
-                                    <div className={styles.iconWrap}>
-                                        <Linens />
-                                        <span className={styles.tooltip}>Постельное белье</span>
                                     </div>
-                                )}
+                                    <div className={styles.costRow}>
+                                        <span className={styles.costValue}>
+                                            {lowerPrice.toLocaleString("ru-RU")}
+                                        </span>
+                                        <Ruble className={styles.rubleIcon} />
 
-                                {coach.have_express && (
-                                    <div className={styles.iconWrap}>
-                                        <Food />
-                                        <span className={styles.tooltip}>Питание</span>
                                     </div>
-                                )}
-                            </div>
+                                </div>
+                            )}
+
+                            {isLuxOrSeat && (
+                                <div className={styles.costRow}>
+                                    <span className={styles.costValue}>
+                                        {lowerPrice.toLocaleString("ru-RU")}
+                                    </span>
+                                    <Ruble className={styles.rubleIcon} />
+
+                                </div>
+                            )}
                         </div>
+
                     </div>
 
-                    {/* ВЕРХНИЕ / НИЖНИЕ */}
-                    <div className={styles.row}>
-                        <div className={styles.col}>
-                            <div className={styles.colTitle}>Верхние</div>
-                            <div className={styles.colValue}>{coach.top_price > 0 ? coach.top_price : "-"}</div>
-                        </div>
+                    {/* === ОБСЛУЖИВАНИЕ === */}
+                    <div className={styles.col}>
+                        <div className={styles.titleGray}>Обслуживание</div>
 
-                        <div className={styles.col}>
-                            <div className={styles.colTitle}>Нижние</div>
-                            <div className={styles.colValue}>{coach.bottom_price > 0 ? coach.bottom_price : "-"}</div>
+                        <div className={styles.serviceIcons}>
+                            {coach.have_air_conditioning && (
+                                <div className={styles.iconWrap}><AC /></div>
+                            )}
+                            {coach.have_wifi && (
+                                <div className={styles.iconWrap}><Wifi /></div>
+                            )}
+                            {!coach.is_linens_included && coach.linens_price > 0 && (
+                                <div className={styles.iconWrap}><Linens /></div>
+                            )}
+                            {coach.have_express && (
+                                <div className={styles.iconWrap}><Food /></div>
+                            )}
                         </div>
-
-                        {coach.side_price > 0 && (
-                            <div className={styles.col}>
-                                <div className={styles.colTitle}>Боковые</div>
-                                <div className={styles.colValue}>{coach.side_price}</div>
-                            </div>
-                        )}
                     </div>
                 </div>
             </div>
+
             {/* КАРТА МЕСТ */}
             <div className={styles.seatMap}>
                 <CarSeatsMap seats={seats} type={coach.class_type} />
