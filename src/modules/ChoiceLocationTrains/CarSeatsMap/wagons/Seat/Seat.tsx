0b70970b06bcd8a06e94c69649154ea593cf8191
Nikita Shvabauer <halkaster666@mail.ru>
2025-11-25 12:32:46 +0500

    правил вид занятых мест

--- src/modules/ChoiceLocationTrains/CarSeatsMap/wagons/Seat/Seat.tsx
+++ src/modules/ChoiceLocationTrains/CarSeatsMap/wagons/Seat/Seat.tsx
@@ -12,13 +12,17 @@ const Seat: React.FC<Props> = ({ number, available, reserved, type }) => {
     return (
         <div
             className={`
-        ${styles.seat}
-        ${type === "upper" ? styles.upperSeat : ""}
-        ${type === "lower" ? styles.lowerSeat : ""}
-        ${type === "side-upper" ? styles.sideUpperSeat : ""}
-        ${type === "side-lower" ? styles.sideLowerSeat : ""}
-        ${reserved ? styles.reserved : available ? styles.free : styles.taken}
-    `}
+                ${styles.seat}
+                ${type === "upper" ? styles.upperSeat : ""}
+                ${type === "lower" ? styles.lowerSeat : ""}
+                ${type === "side-upper" ? styles.sideUpperSeat : ""}
+                ${type === "side-lower" ? styles.sideLowerSeat : ""}
+
+                ${reserved ? styles.reserved : ""}
+                ${!available && !reserved ? styles.unavailable : ""}
+                ${available && !reserved ? styles.free : ""}
+                ${!available && !reserved ? "" : ""} 
+            `}
         >
             {number}
         </div>
