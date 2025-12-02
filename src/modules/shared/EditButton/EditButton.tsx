96df6874525a8ffa47fde5b42a850c40c33a3880
Nikita Shvabauer <halkaster666@mail.ru>
2025-12-02 15:29:32 +0500

    Добавил логику переноса данных в кнопку

--- src/modules/shared/EditButton/EditButton.tsx
+++ src/modules/shared/EditButton/EditButton.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import styles from "./EditButton.module.css";
 
 interface Props {
     target: "train" | "passengers" | "payment";
 }
 
-const routesMap: Record<Props["target"], string> = {
-    train: "/search",
-    passengers: "/passengers",
-    payment: "/payment"
-};
-
 const EditButton: React.FC<Props> = ({ target }) => {
     const navigate = useNavigate();
+    const { state } = useLocation(); // ← ТЕКУЩИЕ ДАННЫЕ СО ВСЕЙ СТРАНИЦЫ
+
+    const routes = {
+        train: "/search",
+        passengers: "/passengers",
+        payment: "/payment",
+    };
 
     return (
         <button
             className={styles.editBtn}
-            onClick={() => navigate(routesMap[target])}
+            onClick={() => navigate(routes[target], { state })}
         >
             Изменить
         </button>
