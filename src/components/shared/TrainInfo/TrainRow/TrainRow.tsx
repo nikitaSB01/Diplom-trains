d6b71ab21328838d9c30b11ced18fea3727babc1
Nikita Shvabauer <halkaster666@mail.ru>
2025-12-07 13:42:09 +0500

    убрал дубль отображение правой карточки поезда с кнопкой выбрать места/изменить

--- src/components/shared/TrainInfo/TrainRow/TrainRow.tsx
+++ src/components/shared/TrainInfo/TrainRow/TrainRow.tsx
@@ -5,7 +5,6 @@ import TrainLeft from "../../TrainLeft/TrainLeft";
 import TrainDirections from "../TrainDirections/TrainDirections";
 import TrainPrices from "../TrainPrices/TrainPrices";
 import TrainServices from "../TrainServices/TrainServices";
-
 import { Train } from "../../../../types/Train/trainTypes";
 
 interface Props {
@@ -13,32 +12,33 @@ interface Props {
     fromCity: any;
     toCity: any;
     onSelectTrain?: (train: Train) => void;
+    actionSlot?: React.ReactNode;
 }
 
-const TrainRow: React.FC<Props> = ({ train, fromCity, toCity, onSelectTrain }) => {
+const TrainRow: React.FC<Props> = ({ train, fromCity, toCity, onSelectTrain, actionSlot }) => {
     const dep = train.departure;
     const arr = train.arrival;
 
     return (
         <div className={styles.row}>
 
-            {/* ЛЕВАЯ КОЛОНКА */}
             <TrainLeft dep={dep} fromCity={fromCity} toCity={toCity} />
 
-            {/* ЦЕНТРАЛЬНЫЙ БЛОК (Туда / Обратно) */}
             <TrainDirections departure={dep} arrival={arr} />
 
-            {/* ПРАВАЯ КОЛОНКА */}
             <div className={styles.right}>
                 <TrainPrices dir={dep} />
-                
+
                 <div className={styles.serviceBut}>
                     <TrainServices dir={dep} />
-                    <button
-                        className={styles.button}
-                        onClick={() => onSelectTrain?.(train)}
-                    > Выбрать места
-                    </button>
+                    {actionSlot ?? (
+                        <button
+                            className={styles.button}
+                            onClick={() => onSelectTrain?.(train)}
+                        >
+                            Выбрать места
+                        </button>
+                    )}
                 </div>
 
             </div>
--- src/modules/shared/TrainSummaryBlock/TrainSummaryBlock.tsx
+++ src/modules/shared/TrainSummaryBlock/TrainSummaryBlock.tsx
@@ -3,12 +3,7 @@ import styles from "./TrainSummaryBlock.module.css";
 
 import TitleBlockReusable from "../TitleBlockReusable/TitleBlockReusable";
 import EditButton from "../../shared/EditButton/EditButton";
-
-// 🔹 общие компоненты поезда
-import TrainLeft from "../../../components/shared/TrainLeft/TrainLeft";
-import TrainDirections from "../../../components/shared/TrainInfo/TrainDirections/TrainDirections";
-import TrainPrices from "../../../components/shared/TrainInfo/TrainPrices/TrainPrices";
-import TrainServices from "../../../components/shared/TrainInfo/TrainServices/TrainServices";
+import TrainRow from "../../../components/shared/TrainInfo/TrainRow/TrainRow";
 
 interface Props {
     orderData: any;
@@ -16,33 +11,21 @@ interface Props {
 }
 
 const TrainSummaryBlock: React.FC<Props> = ({ orderData, onEdit }) => {
-    const dep = orderData.train.departure;
-    const arr = orderData.train.arrival;
+    const train = orderData.train;
 
     return (
         <div className={styles.wrapper}>
             <TitleBlockReusable title="Поезд" />
 
             <div className={styles.content}>
-                {/* ==== ЛЕВАЯ КОЛОНКА (как в списке поездов) ==== */}
-                <TrainLeft
-                    dep={dep}
-                    fromCity={dep.from.city}
-                    toCity={dep.to.city}
-                />
-
-                {/* ==== ЦЕНТРАЛЬНЫЙ БЛОК (туда / обратно) ==== */}
-                <TrainDirections departure={dep} arrival={arr} />
-
-                {/* ==== ПРАВАЯ КОЛОНКА (цены + услуги + кнопка редактировать) ==== */}
-                <div className={styles.right}>
-                    <TrainPrices dir={dep} />
-
-                    <div className={styles.editWrapper}>
-                        <TrainServices dir={dep} />
+                <TrainRow
+                    train={train}
+                    fromCity={train.departure.from.city}
+                    toCity={train.departure.to.city}
+                    actionSlot={
                         <EditButton target="train" onClick={onEdit} />
-                    </div>
-                </div>
+                    }
+                />
             </div>
         </div>
     );
